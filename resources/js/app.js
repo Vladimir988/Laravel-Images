ca02412158ec976bdfc364cce15145415782cf85
Vladimir988 <optimist0706@gmail.com>
2023-06-25 18:51:29 +0300

    start point for app ready

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
+import Vue from 'vue';
+import Index from './components/Index';
 require('./bootstrap');
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        Index
+    }
+});
---
+++ resources/views/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.index')
+@section('content')
+    <Index></Index>
+@endsection
