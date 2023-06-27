dcc9275e603003676ab9ee66bded3c11c5de010d
Vladimir988 <optimist0706@gmail.com>
2023-06-27 20:56:20 +0300

    dropzone js

--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,12 +1,25 @@
 <template>
-    <div>
-        Index
+    <div class="w-75">
+        <div ref="dropzone" class="btn p-5 bg-info d-block text-center text-light">
+            Upload
+        </div>
     </div>
 </template>
 
 <script>
+import Dropzone from 'dropzone';
 export default {
-    name: "Index"
+    name: "Index",
+    data() {
+        return {
+            dropzone: null,
+        }
+    },
+    mounted() {
+        this.dropzone = new Dropzone(this.$refs.dropzone, {
+            url: "/file/post"
+        });
+    }
 }
 </script>
 
